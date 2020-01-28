f853e8b5912e3617ed612b979b589d5e7557433c
Henry Gammad <Henryj4796@yahoo.com>
2020-01-28 16:26:34 -0600

    Fixing guards, level setup, adding final tutorial boards etx

---
+++ Assets/Scripts/ButtonHighlight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonHighlight : MonoBehaviour
+{
+    public Image ButtonHighlighted;
+
+    public void OnMouseEnter(){
+        ButtonHighlighted.GetComponent<Image>().color = new Color(0.70f, 0.70f, 0.70f, 1);
+        //Debug.Log("Mouse Entered");
+    }
+    public void OnMouseExit(){
+        ButtonHighlighted.GetComponent<Image>().color = Color.white;
+    }
+}
